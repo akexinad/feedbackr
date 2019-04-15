237a1f0973da8fbb9163712a0067d53fbcc4a15f
danny <danixeka@gmail.com>
2019-04-15 13:16:34 +1000

    getting access to user info when they log in

--- src/index.js
+++ src/index.js
@@ -13,19 +13,21 @@ passport.use(
     clientID: googleClientID,
     clientSecret: googleClientSecret,
     callbackURL: '/auth/google/callback',
-  }, (accessToken) => {
-    console.log(accessToken);
+  }, (accessToken, refreshToken, profile, done) => {
+    console.log('accessToken', accessToken);
+    console.log('refreshToken', refreshToken);
+    console.log('profile', profile);
   })
 )
 
 app.get(
-  '/auth/google', passport.authenticate('google', {
-    scope: [
-      'profile',
-      'email'
-    ]
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
   })
 )
+
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 app.listen(PORT, () => {
   console.log(`Server is up on http://localhost:${ PORT }`)
