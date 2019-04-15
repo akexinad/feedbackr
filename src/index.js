3709376f4e3b218867ea3023d86bff12c21ea5ff
danny <danixeka@gmail.com>
2019-04-15 11:48:11 +1000

    setup googleOAuth

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-const express = require('express')
 // The reason we use require instead of import is because import belongs to ES2015 and node does not support that YET
+const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+
+const { clientId, clientSecret } = require('../config/googleOAuth.js')
 
 const PORT = process.env.PORT
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send({
-    hello: "world"
-  })
-})
+// passport.use(new GoogleStrategy())
 
 app.listen(PORT, () => {
   console.log(`Server is up on http://localhost:${ PORT }`)
