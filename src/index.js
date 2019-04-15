9f3414facf378cda65734e5bb3c8bcc08e782f71
danny <danixeka@gmail.com>
2019-04-16 06:51:49 +1000

    1 - Added cookie sessions. 2 - Changed database over to localhost bc its quicker to see data on robo 3t than to wait for my shitty internet connection to load up mongo atlas everytime.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,32 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cookeSession = require('cookie-session')
+const passport = require('passport')
 
 require('../models/User.js')
-
+const authRoutes = require('../routes/authRoutes.js')
 require('../services/passport.js')
+const { mongoURI, cookieKey } = require('../config/keys.js')
 
-const { mongoURI } = require('../config/keys.js')
-
-mongoose.connect(mongoURI, {
-  useNewUrlParser: true
+mongoose.connect('mongodb://127.0.0.1:27017/feedbackr-api', {
+  useNewUrlParser: true,
+  useCreateIndex: true, // Indexes the data in mongodb, allowing for quicker access.
+  useFindAndModify: false
 })
 
 const PORT = process.env.PORT
-
 const app = express()
 
-require('../routes/authRoutes.js')(app)
+app.use(
+  cookeSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [cookieKey]
+  })
+)
+app.use(passport.initialize())
+app.use(passport.session())
+
+authRoutes(app)
 
 app.listen(PORT, () => {
   console.log(`Server is up on http://localhost:${ PORT }`)
