54b3a4b98c75ab02e46a3aa917bf0eabcc1a153b
danny <danixeka@gmail.com>
2019-04-15 12:31:55 +1000

    getting oauth error message 400

--- src/index.js
+++ src/index.js
@@ -3,12 +3,29 @@ const express = require('express')
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 
-const { clientId, clientSecret } = require('../config/googleOAuth.js')
+const { googleClientID, googleClientSecret } = require('../config/googleOAuth.js')
 
 const PORT = process.env.PORT
 const app = express()
 
-// passport.use(new GoogleStrategy())
+passport.use(
+  new GoogleStrategy({
+    clientID: googleClientID,
+    clientSecret: googleClientSecret,
+    callbackURL: '/auth/google/callback',
+  }, (accessToken) => {
+    console.log(accessToken);
+  })
+)
+
+app.get(
+  '/auth/google', passport.authenticate('google', {
+    scope: [
+      'profile',
+      'email'
+    ]
+  })
+)
 
 app.listen(PORT, () => {
   console.log(`Server is up on http://localhost:${ PORT }`)
