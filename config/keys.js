f2220b5446e88e280b6bba261c7d9cf684407a03
danny <danixeka@gmail.com>
2019-04-16 17:42:34 +1000

    database not responding anymore, rebuilding cluster

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,15 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./prod.js')
-} else {
-  module.exports = require('./dev.js')
+// if (process.env.NODE_ENV === 'production') {
+//   module.exports = require('./prod.js')
+// } else {
+//   module.exports = require('./dev.js')
+// }
+
+module.exports = {
+  googleClientID: '306362386184-5nfj550o513kbkdscp1o8hitr7v3nq74.apps.googleusercontent.com',
+  googleClientSecret: 'RpiP7NtiW9HXdDAJ1VrbWCUf',
+  mongodbUri: 'mongodb://127.0.0.1:27017/feedbackr-api',
+
+  mongodbAtlasUri: 'mongodb+srv://dannymongodb:zI5HP1XhUHjdqtdP@feedbackr-api-680p5.mongodb.net/test?retryWrites=true',
+
+  cookieKey: '3jk487df9s76g78g31243v12kj3h41y40sdf70sd870./.,[l[];;]1kve7183he'
 }
