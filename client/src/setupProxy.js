f5de72b4a89377f35f1f7fca7c8f653d9e77707e
danny <danixeka@gmail.com>
2019-04-22 10:54:54 +1000

    basic setup

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,9 +1,9 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = (app) => {
-    app.use(proxy(['/api', '/auth/google'],
-        {
-            target: 'http://localhost:3001'
-        }
-    ))
+  app.use(proxy(['/api', '/auth/google'],
+    {
+      target: 'http://localhost:3001'
+    }
+  ))
 };
