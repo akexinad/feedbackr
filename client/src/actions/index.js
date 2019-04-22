744ef43c6cf528157a886dc2c87c76a4f6c2eb39
danny <danixeka@gmail.com>
2019-04-22 14:16:14 +1000

    passing fetchUser into the authReducer

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 import { FETCH_USER } from './types.js';
 
 export const fetchUser = () => async dispatch => {
-  const response = await axios.get('/api/current_user');
+  const res = await axios.get('/api/current_user');
 
   dispatch({
     type: FETCH_USER,
-    payload: response
+    payload: res.data
   });
 };
