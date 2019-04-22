d7183ca28bdf72fa6ac920b55c585f2260245df5
danny <danixeka@gmail.com>
2019-04-22 12:32:54 +1000

    basic router configuration

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-export default class App extends React.Component {
+import Header from './Header.js'
 
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveryNew = () => <h2>SurveryNew</h2>
+const Landing = () => <h2>Landing</h2>
+
+export default class App extends Component {
   render() {
     return(
       <div>
-        Hello World
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveryNew} />
+          </div>
+        </BrowserRouter>
       </div>
     )
   }
-
 }
