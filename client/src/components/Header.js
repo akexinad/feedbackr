c0c89d9ac43e571674639234f9542fa76c01b14a
danny <danixeka@gmail.com>
2019-04-22 15:32:57 +1000

    condition logo link logic

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   renderContent() {
@@ -34,12 +35,12 @@ class Header extends Component {
     return(
       <nav>
         <div className="nav-wrapper">
-          <a
-            href="/"
+          <Link
+            to={ this.props.auth ? '/surveys' : '/' }
             className="left brand-logo"
           >
             FeedBackr
-          </a>
+          </Link>
           <ul className="right">
             { this.renderContent() }
           </ul>
