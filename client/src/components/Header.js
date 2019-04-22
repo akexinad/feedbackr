f61d12bb0964fe59601f0cd74d08df79cea9efc6
danny <danixeka@gmail.com>
2019-04-23 08:24:54 +1000

    added css properties to pay button

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -11,19 +11,12 @@ class Header extends Component {
         return;
 
       case false:
-        return(<li>
-            <a
-              href="/auth/google"
-            >
-              Login With Google
-            </a>
-          </li>
-        );
+        return <li><a href="/auth/google">Login With Google</a></li>
 
       default:
         return[
-          <li><Payments /></li>,
-          <li><a href="/api/logout">Logout</a></li>
+          <li key="1" ><Payments /></li>,
+          <li key="2" ><a href="/api/logout">Logout</a></li>
         ];
     }
   }
--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -6,10 +6,14 @@ class Payments extends Component {
   render() {
     return(
       <StripeCheckout
+        name="FeedBackr"
+        description="USD 5 x 5 email credits"
         amount={500}
         token={token => console.log(token)}
         stripeKey={process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}
-      />
+      >
+        <button className="btn">ADD CREDITS</button>
+      </ StripeCheckout>
     )
   }
 }
