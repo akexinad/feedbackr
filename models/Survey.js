f6dede5797c15448b3aaa89c95aed8063c396161
danny <danixeka@gmail.com>
2019-04-24 08:23:14 +1000

    setting up sub documents

--- models/Survey.js
+++ models/Survey.js
@@ -1,12 +1,21 @@
 const mongoose = require('mongoose');
-
 const { Schema } = mongoose;
+
+const RecipientSchema = require('./Recipient.js');
 
 const surveySchema = new Schema({
   title: String,
   body: String,
   subject: String,
-  recipients: [String]
+  recipients: [RecipientSchema],
+  yes: {
+    Type: Number,
+    default: 0
+  },
+  no: {
+    Type: Number,
+    default:0
+  }
 });
 
 mongoose.model('surveys', surveySchema);
