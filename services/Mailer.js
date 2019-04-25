ead7f2403ca2c61419b482067623639ba159d6e1
danny <danixeka@gmail.com>
2019-04-25 13:22:53 +1000

    added send function and api key to Mailer class

--- services/Mailer.js
+++ services/Mailer.js
@@ -6,6 +6,7 @@ class Mailer extends helper.Mail {
   constructor({ subject, recipients }, content) {
     super();
 
+    this.sgApi = sendgrid(keys.sendGridKey);
     this.from_email = new helper.email('no-reply@feedbackr.com');
     this.subject = subject;
     this.body = new helper.Content('text/html', content);
@@ -39,4 +40,18 @@ class Mailer extends helper.Mail {
 
     this.addPersonalization(personalise);
   }
+
+  async send() {
+    const request = this.sgApi.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: this.toJSON()
+    });
+
+    const response = this.sgApi.API(request);
+
+    return response;
+  }
 }
+
+module.exports = Mailer;
