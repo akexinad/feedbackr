f1e8847e22c275799f580a9af34e8122c6948a44
danny <danixeka@gmail.com>
2019-04-27 09:48:23 +1000

    adding react router link to dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Dashboard() {
   return (
     <div>
-      Dashboard
+      <h2>Dashboard</h2>
       <div className="fixed-action-btn">
-        <a href="" className="btn-floating btn-large red">
+        <Link to="/surveys/new" className="btn-floating btn-large red">
           <i className="material-icons">add</i>
-        </a>
+        </Link>
       </div>
     </div>
   )
