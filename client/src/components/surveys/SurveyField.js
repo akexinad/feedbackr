112074fa0e5b85ebc3dc4fd6d3d0e406b188b360
danny <danixeka@gmail.com>
2019-04-27 12:10:26 +1000

    passing all of the input props in to the input tag insie the surveyField component

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,10 +1,11 @@
 // SurveyField conatins logic to render a single label and text input.
 import React from 'react';
 
-export default () => {
+export default ({ input }) => {
+  console.log(input);
   return (
     <div>
-      <input />
+      <input {...input} />
     </div>
   )
 }
