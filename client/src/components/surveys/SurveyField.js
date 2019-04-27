58a994447da0b490762cc40608fede1aca1e48a6
danny <danixeka@gmail.com>
2019-04-27 12:15:59 +1000

    sccssfly logging out all fields

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,10 +1,11 @@
 // SurveyField conatins logic to render a single label and text input.
 import React from 'react';
 
-export default ({ input }) => {
+export default ({ input, label }) => {
   console.log(input);
   return (
     <div>
+      <label>{label}</label>
       <input {...input} />
     </div>
   )
