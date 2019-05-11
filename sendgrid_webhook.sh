4dd6cbf1174fa216680c852a1d394d8a4ea7a35c
danny <danixeka@gmail.com>
2019-05-12 09:41:04 +1000

    comments

--- sendgrid_webhook.sh
+++ sendgrid_webhook.sh
@@ -1,4 +1,5 @@
 function localtunnel {
+  # NOTE: the url in https://app.sendgrid.com/settings/mail_settings will need to be changed when deployed in production:
   lt -s bdefhsakxdciqpow --port 3001
 }
 until localtunnel; do
