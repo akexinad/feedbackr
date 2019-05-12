77cfd31df6ef4cc439cc9271b68851770dea72b4
danny <danixeka@gmail.com>
2019-05-12 10:18:25 +1000

    webhook comments

--- sendgrid_webhook.sh
+++ sendgrid_webhook.sh
@@ -1,5 +1,7 @@
 function localtunnel {
-  # NOTE: the url in https://app.sendgrid.com/settings/mail_settings will need to be changed when deployed in production:
+  # NOTE: webhooks will not work in production.
+  # the url in https://app.sendgrid.com/settings/mail_settings will need to be changed when deployed in production:
+  # According to Grider you need to make a second sendgrid account
   lt -s bdefhsakxdciqpow --port 3001
 }
 until localtunnel; do
